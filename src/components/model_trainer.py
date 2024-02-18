2b2254d1111b103116a8507e676f4d4501b7f053
Taukeer <Taukeersurrey@gmail.com>
2024-02-18 20:40:40 +0000

    added prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,11 +18,9 @@ from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
-
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
-
 
 class ModelTrainer:
     def __init__(self):
@@ -44,9 +42,9 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
+                
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-             
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -85,6 +83,39 @@ class ModelTrainer:
                 
             }
 
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+
+
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
          
          
             
--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,11 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-from src.exception import CustomException
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -16,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -52,3 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
